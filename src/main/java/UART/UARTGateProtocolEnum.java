a15865ed4b335658da0b5406718200ca536c4d68
Boris Chirkov <b.v.chirkov@udsu.ru>
2017-01-19 12:41:15 +0400

    Init 	new file:   .gitignore 	new file:   pom.xml 	new file:   src/main/java/SystemMain.java 	new file:   src/main/java/UART/UARTGateProtocolEnum.java 	new file:   src/main/java/UART/UARTPackage.java

---
+++ src/main/java/UART/UARTGateProtocolEnum.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (C) 2017 Ushakov Danil
+ *
+ * Project website:       http://eesystem.ru
+ * Organization website:  http://rintd.ru
+ *
+ * --------------------- DO NOT REMOVE THIS NOTICE -----------------------------
+ * UARTGateProtocolEnum is part of WirelessSystemUARTServer.
+ *
+ * WirelessSystemUARTServer is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * WirelessSystemUARTServer is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with WirelessSystemUARTServer. If not, see <http://www.gnu.org/licenses/>.
+ * -----------------------------------------------------------------------------
+ *
+ * This code is in BETA; some features are incomplete and the code
+ * could be written better.
+ */
+
+package UART;
+
+/**
+ * Created by Jiro on 18.01.17.
+ */
+public class UARTGateProtocolEnum {
+    public static char PACKAGE_ERROR      = 0x00;//00; //           just empty;
+    public static char PACKAGE_ECHO       = 0x0A;//10; //           just return [ADRESS,1];
+    public static char PACKAGE_ECHO_STATE = 0x0B;//11; //           just return [ADRESS,BATTERY,TEMP];
+
+    public static char PACKAGE_GET_NODE_CONF     = 0x14;//20; //           just return;
+    public static char PACKAGE_INIT_NODE_CONF    = 0x15;//21; //           just update NODE [ADDRESS,ect., ...];
+    public static char PACKAGE_UPDATE_NODE_CONF  = 0x16;//22; //           just update NODE [ADDRESS,ect., ...];
+    public static char PACKAGE_SET_NODE_ADDRESS  = 0x17;//23; //
+    public static char PACKAGE_NEED_SET_ADDRESSS = 0x18;//24; //
+
+    public static char PACKAGE_GET_ARROW_STATE_COUNT = 50;
+    public static char PACKAGE_SET_ARROW_STATE       = 51;
+}
---
+++ src/main/java/UART/UARTPackage.java
@@ -0,0 +1,140 @@
+/*
+ * Copyright (C) 2017 Ushakov Danil
+ *
+ * Project website:       http://eesystem.ru
+ * Organization website:  http://rintd.ru
+ *
+ * --------------------- DO NOT REMOVE THIS NOTICE -----------------------------
+ * UARTPackage is part of WirelessSystemUARTServer.
+ *
+ * WirelessSystemUARTServer is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * WirelessSystemUARTServer is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with WirelessSystemUARTServer. If not, see <http://www.gnu.org/licenses/>.
+ * -----------------------------------------------------------------------------
+ *
+ * This code is in BETA; some features are incomplete and the code
+ * could be written better.
+ */
+
+package UART;
+
+/**
+ * Created by Jiro on 18.01.17.
+ */
+public class UARTPackage {
+    private static int RX_STAGE_WAIT_FOR_BEGIN = 0;
+    private static int RX_STAGE_READ_HEADER    = 1;
+    private static int RX_STAGE_READ_BODY      = 2;
+    private static char sRX_headerData[];
+    private static int sRX_headerDataSize = 5;
+    private static int sRX_bodyDataSize   = 0;
+    private static char sRX_bodyData[];
+    private static int sRX_isEscapeMode = 0;
+    public int sourceAddress;
+    public int destAddress;
+    public char type = 0x00;
+    public  byte[]            data;
+    private UARTMessageHeader header;
+
+    public static UARTPackage getPackegeFromBytes(byte[] bytes) {
+
+        int uart_state = RX_STAGE_WAIT_FOR_BEGIN;
+
+        for (int i = 0; i < bytes.length; i++) {
+            char value = (char) bytes[i];
+
+            switch (uart_state) {
+            case 0:
+                if (value == 0xFF) {
+                    uart_state = RX_STAGE_READ_HEADER;
+                    sRX_headerDataSize = 0;
+                    sRX_bodyDataSize = 0;
+                    sRX_isEscapeMode = 0;
+                }
+                break;
+            case 1:
+                if (sRX_isEscapeMode == 1) {
+                    sRX_isEscapeMode = 0;
+                } else {
+                    if (value == (char) 0xFE)//Escape byte
+                    {
+                        sRX_isEscapeMode = 1;
+                        break;
+                    } else if (value == (char) 0xFF)//New message
+                    {
+                        uart_state = RX_STAGE_READ_HEADER;
+                        sRX_headerDataSize = 0;
+                        sRX_bodyDataSize = 0;
+                        sRX_isEscapeMode = 0;
+                        break;
+                    }
+                }
+
+                sRX_headerData[sRX_headerDataSize++] = (char) value;
+
+                if (sRX_headerDataSize >= sRX_headerDataSize) {
+                    //TODO: дописать обработку хедеров
+                    if (header -> size > 0) {
+                        uart_state = RX_STAGE_READ_BODY;
+                        sRX_bodyDataSize = 0;
+                    } else {
+                        uart_state = RX_STAGE_WAIT_FOR_BEGIN;
+                    }
+                }
+
+                break;
+            case 2:
+                if (sRX_isEscapeMode == 1) {
+                    sRX_isEscapeMode = 0;
+                } else {
+                    if (value == 0xFE)//Escape byte
+                    {
+                        sRX_isEscapeMode = 1;
+                        break;
+                    } else if (value == 0xFF)//New message
+                    {
+                        uart_state = RX_STAGE_READ_HEADER;
+                        sRX_headerDataSize = 0;
+                        sRX_bodyDataSize = 0;
+                        sRX_isEscapeMode = 0;
+                        break;
+                    }
+                }
+
+                sRX_bodyData[sRX_bodyDataSize++] = value;
+
+                //TODO: дописать обработку хедеров
+                // UartMsgHeader_t header;
+                // memcpy(&header,sRX_headerData,sizeof(UartMsgHeader_t));
+
+                if (sRX_bodyDataSize >= header.size) {
+                    uart_state = RX_STAGE_WAIT_FOR_BEGIN;
+                    sRX_headerDataSize = 0;
+                    sRX_bodyDataSize = 0;
+                    sRX_isEscapeMode = 0;
+                }
+
+                break;
+            }
+        }
+
+        return null;
+    }
+
+    public class UARTMessageHeader {
+        int length = 0;
+        int from = 0;
+        int to   = 0;
+
+        public UARTMessageHeader() {}
+    }
+}
